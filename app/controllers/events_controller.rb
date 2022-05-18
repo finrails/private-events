f430d29e65b7208ad0212a368bb825e98276ae92
finrails <macacofinn@yahoo.com>
2022-05-18 14:18:34 -0400

    add: new event view, controller and route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
   def index
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to root_path, notice: "Your event have been created."
+    else
+      flash.now[:alert] = "Some inputs are invalid, please check it out!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User",
                        foreign_key: "creator_id"
+  validates :date, presence: true, length: { in: 6..20 }
 end

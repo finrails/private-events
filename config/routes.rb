8a32c1a97da3508f08f00f1b808fb3be6022ad2a
finrails <macacofinn@yahoo.com>
2022-05-18 13:55:15 -0400

    add: user show created events view, route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events, only: %i[ index ]
+  resources :users, only: %i[ show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

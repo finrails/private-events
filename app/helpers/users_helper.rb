825d40b0662e1b1bd746e526a3bb41c22a91f030
finrails <macacofinn@yahoo.com>
2022-05-23 13:39:49 -0400

    add: already in button in events page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def in_attendance?(user_id: nil, event_id: nil)
+    user_attendance_query = EventAttendance.where(attendee_id: user_id, attendeed_event_id: event_id).first
+
+    return true if user_attendance_query
+    false
+  end
 end

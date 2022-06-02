94930f3f7ba54c3a3b3b7ed4d6e724c80698e4c6
finrails <macacofinn@yahoo.com>
2022-06-02 14:53:57 -0400

    upt: profile button and some small tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events, only: %i[ edit update index new create ]
   resources :users, only: %i[ show ]
+
   put 'accept/invitation/:invitation_id', controller: :users, action: 'accept_invitation', as: :accept_invitation
 
   resources :events do
     member do
+      delete 'unattend'
       post 'invite/:user_id', action: 'invite', as: :invite_user
     end
 
     resources :attendances, only: %i[ destroy create ]
-
-    member do
-      delete 'unattend'
-    end
 
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
